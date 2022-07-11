9d3607a5a4d4aec16d436a3699e2858b06b9cb9c
Nacnano <chotpisit080916c@gmail.com>
2022-07-11 03:29:31 +0000

    update file path

--- dags/operators/process_data.py
+++ dags/operators/process_data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-file_path = "data/anime.csv"
+file_path = "/home/nacnano/Documents/github/mal-data-warehouse/data/anime.csv"
 df = pd.read_csv(file_path, delimiter=",")
 new_df = df[df["members"] < 200000]
 
-end_path = "data/unheard_anime.csv"
+end_path = "/home/nacnano/Documents/github/mal-data-warehouse/data/unheard_anime.csv"
 new_df.to_csv(end_path, index=False)
--- dags/request.py
+++ dags/request.py
@@ -1,7 +1,5 @@
 from datetime import datetime, timedelta
 import os
-# from dotenv import load_dotenv
-# load_dotenv()
 
 from airflow import DAG
 from airflow.models import Variable
@@ -10,37 +8,37 @@ from airflow.utils.dates import days_ago
 
 
 default_args = {
-    'owner': 'airflow',
+    'owner': 'nacnano',
     'depends_on_past': False,
-    'retries': 4,
-    'retry_delay': timedelta(seconds=15),
+    'retries': 5,
+    'retry_delay': timedelta(seconds=1),
 }
 with DAG(
     'anime-data-warehouse',
     default_args=default_args,
     description='A request for anime database from API',
     schedule_interval=timedelta(minutes=1),
-    start_date=days_ago(1),
+    start_date=days_ago(0),
     catchup=False,
     tags=['anime', 'myanimelist', 'mal', 'anilist']
 ) as dag:
 
-    setup_folder = BashOperator(
-        task_id = 'setup-folder-directory',
-        # bash_command = f'cd {os.getenv("FOLDER_DIRECTORY")}'
-        bash_command = 'cd Documents/github/mal-data-warehouse' # <-- Delete this after succesfully debug dotenv python setup - @Nacnano
-    )
+    # setup_folder = BashOperator(
+    #     task_id = 'setup-folder-directory',
+    #     # bash_command = f'cd {os.getenv("FOLDER_DIRECTORY")}'
+    #     bash_command = 'cd Documents/github/mal-data-warehouse' # <-- Delete this after succesfully debug dotenv python setup - @Nacnano
+    # )
     fetch_static_api = BashOperator(
         task_id='fetch-static-api', 
-        bash_command='python3 /dags/operators/temporary_data.py'
+        bash_command='python3 /home/nacnano/Documents/github/mal-data-warehouse/dags/operators/temporary_data.py'
     )
     fetch_mal_api = BashOperator(
         task_id='fetch-mal-api',
-        bash_command='python3 /dags/operators/fetch_api.py'
+        bash_command='python3 /home/nacnano/Documents/github/mal-data-warehouse/dags/operators/fetch_api.py'
     )
     process_data= BashOperator(
         task_id='process_data',
-        bash_command='python3 /dags/operators/process_data.py'
+        bash_command='python3 /home/nacnano/Documents/github/mal-data-warehouse/dags/operators/process_data.py'
     )
 
-setup_folder >> fetch_static_api >> process_data >> fetch_mal_api
+fetch_static_api >> process_data
